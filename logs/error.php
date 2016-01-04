270ce108a05f8a88190cb5819b6d1d435b230de9
Lewis Jang <lewis.aedes@gmail.com>
2016-01-04 09:04:32 +0000

    init

---
+++ logs/error.php
@@ -0,0 +1,20 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2014-08-25 05:26:56 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority	category	message
+2014-08-25T05:26:56+00:00	INFO	joomlafailure	帳號與密碼不正確，或是您沒有帳號
+2014-08-25T05:27:02+00:00	INFO	joomlafailure	帳號與密碼不正確，或是您沒有帳號
+2014-08-26T08:16:17+00:00	INFO	joomlafailure	帳號與密碼不正確，或是您沒有帳號
+2014-08-26T08:16:24+00:00	INFO	joomlafailure	帳號與密碼不正確，或是您沒有帳號
+2014-09-05T07:38:30+00:00	INFO	joomlafailure	帳號與密碼不正確，或是您沒有帳號
+2014-09-05T07:38:31+00:00	INFO	joomlafailure	帳號與密碼不正確，或是您沒有帳號
+2014-09-18T10:33:23+00:00	INFO	joomlafailure	帳號與密碼不正確，或是您沒有帳號
+2014-09-23T09:51:49+00:00	INFO	joomlafailure	帳號與密碼不正確，或是您沒有帳號
+2014-10-13T08:33:53+00:00	INFO	joomlafailure	帳號與密碼不正確，或是您沒有帳號
+2014-10-22T02:27:32+00:00	INFO	joomlafailure	帳號與密碼不正確，或是您沒有帳號
+2014-12-23T14:40:08+00:00	INFO	joomlafailure	帳號與密碼不正確，或是您沒有帳號
+2014-12-24T08:12:36+00:00	INFO	joomlafailure	帳號與密碼不正確，或是您沒有帳號
+2015-01-31T08:10:00+00:00	INFO	joomlafailure	帳號與密碼不正確，或是您沒有帳號
+2015-03-13T03:57:08+00:00	INFO	joomlafailure	不允許空白密碼
